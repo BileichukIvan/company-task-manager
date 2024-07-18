53fafb4a8610139a5de89d5ae5dcdd6cc7b28762
ivanb <bileichuk.ivan@gmail.com>
2024-07-18 18:05:28 +0300

    add db models

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    members = models.ManyToManyField("Worker", related_name="teams")
+    project = models.ManyToManyField(Project, related_name="teams")
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    task_completed = models.ManyToManyField("Task", related_name="completed_by", blank=True)
+    tasks_not_completed = models.ManyToManyField("Task", related_name="not_completed_by", blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name}, {self.last_name})"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("urgent", "Urgent"),
+        ("high", "High"),
+        ("medium", "Medium"),
+        ("low", "Low"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=40, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assigned = models.ManyToManyField(Worker, related_name="assigned_tasks")
+    tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
