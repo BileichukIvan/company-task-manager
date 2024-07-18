b0e7ef4782a24f92141272c30cf6cecaa079b1d9
ivanb <bileichuk.ivan@gmail.com>
2024-07-18 21:37:10 +0300

    add admin for manager app

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import (
+    Worker,
+    Task,
+    Project,
+    TaskType,
+    Tag,
+    Position,
+)
 
-# Register your models here.
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    search_fields = ("username",)
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                        "task_completed",
+                        "tasks_not_completed"
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("is_completed",)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+admin.site.register(TaskType)
+admin.site.register(Tag)
+admin.site.register(Position)
